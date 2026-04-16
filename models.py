8a3117532af8dcd28de30e26c04d2d537176e854
burgernod <damir.bausana@gmail.com>
2026-04-16 15:30:19 +0500

    Made changes to models.py to hopefully fix user registration and login

--- models.py
+++ models.py
@@ -7,16 +7,17 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, nullable=True)    # Имя
-    last_name = Column(String, nullable=True)     # Фамилия
-    username = Column(String, unique=True, index=True, nullable=False) # Псевдоним
-    email = Column(String, unique=True, index=True, nullable=False)    # Email
-    hashed_password = Column(String, nullable=False) # Зашифрованный пароль
-    
-    role = Column(String, default="Member") # Admin, Member, Reviewer
-    is_active = Column(Boolean, default=False) # Станет True после ввода OTP кода
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(String, default="Member")
+    is_active = Column(Boolean, default=False)
 
     tasks = relationship("Task", back_populates="assignee")
+
+class Project(Base):
     __tablename__ = "projects"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -29,8 +30,8 @@ class ColumnModel(Base):
     __tablename__ = "columns"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False) # Например: "To Do", "In Progress"
-    wip_limit = Column(Integer, nullable=False, default=0) # КЛЮЧЕВАЯ ФИЧА КУРСОВОЙ (0 = без лимита)
+    title = Column(String, nullable=False)
+    wip_limit = Column(Integer, nullable=False, default=0)
     project_id = Column(Integer, ForeignKey("projects.id"))
 
     project = relationship("Project", back_populates="columns")
@@ -42,11 +43,11 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True, nullable=False)
     description = Column(Text, nullable=True)
-    priority = Column(String, default="Medium") # High, Medium, Low (Раздел 3.4.5)
+    priority = Column(String, default="Medium")
     deadline = Column(DateTime, default=datetime.datetime.utcnow)
     
     column_id = Column(Integer, ForeignKey("columns.id"))
-    assignee_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    assignee_id = Column(Integer, ForeignKey("users.id"), nullable=True) # ИМЕННО ЭТА СТРОКА ТЕРЯЛАСЬ!
 
     column = relationship("ColumnModel", back_populates="tasks")
     assignee = relationship("User", back_populates="tasks")
